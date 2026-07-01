33d5fbbb770b78bc5ce8444321a9031ce0ce5b14
fardeenpathan <md.fardeen5236@gmail.com>
2026-07-01 13:02:16 +0530

    update

--- templateData/hitchedDefaultData.js
+++ templateData/hitchedDefaultData.js
@@ -15,6 +15,30 @@ const hitchedDefaultData = {
 
   noteText: "Your presence is the greatest gift to us.",
 
+  coupleMessageTitle: "A message from the couple",
+  coupleMessageDescription:
+    "From different traditions to one beautiful journey, join us as we celebrate love, laughter, and forever. This moment wouldn’t be the same without the people we love most. Thank you for your love, blessings, and for making our journey even more special, we’re so excited to celebrate together!",
+  coupleMessageThingsToKnowTitle: "Things to know",
+  coupleMessageThingsToKnowDescription:
+    "To help you feel at ease and enjoy every moment of the celebrations, we’ve gathered a few thoughtful details we’d love for you to know before the big day.",
+  coupleMessageClosingTitle: "Looking Forward to Seeing You",
+  coupleMessageRsvpText: "Click on the Whatsapp icon to RSVP",
+  coupleMessageLocationTitle: "Location",
+  coupleMessageLocationDetails:
+    "The Central Park Hotel\nBund Garden Road,\nAgarkar Nagar, Pune,\nMaharashtra, 411001",
+  coupleMessageWeatherTitle: "Weather",
+  coupleMessageWeatherDetails:
+    "Clouds may drop by uninvited, but so will great vibes and better dance moves. A little rain never stopped a good celebration anyway.",
+  coupleMessageParkingTitle: "Parking",
+  coupleMessageParkingDetails:
+    "Valet parking for all our guests will be available at the venue.",
+  coupleMessageRouteLink: "https://maps.app.goo.gl/fKxi3eDGsTSd5Aaz6?g_st=ic",
+  coupleMessageCarouselImages: [],
+  marriageCountdownTitle: "The countdown begins",
+  marriageCountdownDate: "2026-09-21",
+  marriageCountdownDescription:
+    "Our families are excited that you are able to join us in celebrating what we hope will be one of the happiest days of our lives.",
+
   whatsappNumber: "+919876543210",
 
   rsvpFields: [
