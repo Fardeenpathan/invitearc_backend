757cd4eef8022fb857889bf73143d958cac23a19
fardeenpathan <md.fardeen5236@gmail.com>
2026-07-23 17:22:21 +0530

    update niqah template

--- templateData/niqahDefaultData.js
+++ templateData/niqahDefaultData.js
@@ -1,5 +1,88 @@
 const niqahDefaultData = {
+groomName: "FARDEEN",
+  brideName: "ZARIN",
+  religiousMantra: "بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْم",
+  brideGrandParentsName: "Mrs. Fatima Begum & Mr. Zafar Ahmed",
+  blessingMessage: "With the heavenly blessings of",
+  groomDetails: "(Son of Mrs. Shabana Khan & Mr. Rehan Malik)",
+  brideDetails: "(Daughter of Mrs. Akram Sheikh & Fatima Sheikh)",
+  eventIntro: "On the following events",
+  headline: "INVITES",
+  inviteLine: "you to join us in the wedding celebrations of",
+  thankyoutitle: "With Love From Us",
+  thankyoumessage:
+    "Thank you for being part our journey. Your presence makes this celebration truly meaningful, and we look forward to sharing these cherished moments with you.",
+  
+coupleMessageTitle: "Introducing",
+  coupleMessageDescription: "The Couple",
+  coupleMessageThingsToKnowTitle: "A Guide for Guests",
+  coupleMessageStaffTitle: "Staff",
+  coupleMessageStaffDetails:
+    "for those traveling from far, Royal Orchid Suites offers a comfortable stay nearby.",
+  coupleMessageWeatherTitle: "Weather",
+  coupleMessageWeatherDetails:
+    "Clouds may drop by uninvited, but so will great vibes and better dance moves. A little rain never stopped a good celebration anyway.",
+  coupleMessageParkingTitle: "Parking",
+  coupleMessageParkingDetails:
+    "Valet parking for all our guests will be available at the venue.",
+  coupleMessageThingsToKnowDescription:
+    "Your presence means the world to us. To make your experience effortless and enjoyable, we've gathered a few useful details below.",
+  coupleMessageClosingTitle: "Awaiting the Pleasure of Your Company",
+  coupleMessageRsvpText: "Share Your RSVP",
+  rsvpMode: "whatsapp",
+  Logo: "",
+  marriageCountdownTitle: "The Journey Begins",
+  marriageCountdownDate: "2026-09-21",
+  marriageCountdownDescription:
+    "Surrounded by family and friends, we can't wait to celebrate this beautiful moment with you.",
 
+
+   events : [
+    {
+      title_ceremony: "Mehendi",
+      image: "https://res.cloudinary.com/drl4fmhrq/image/upload/v1784716491/mehendi.0g4oirx0fp_c1_zghaqb.webp",
+      date: "Saturday, April 18th 2026",
+      time: "Join Us at 11 am",
+      venue: "The Leela Palace, Udaipur",
+       venue_address:
+        "Bund Garden Road, Agarkar Nagar\nPune, Maharashtra, 411001",
+      theme: "Glitz & Glam: Shimmer, sequins & Statement Fits",
+      link: "https://maps.app.goo.gl/53z68ksx4cYgoNm59",
+    },
+
+    {
+      title_ceremony: "Nikah",
+      image: "https://res.cloudinary.com/drl4fmhrq/image/upload/v1784716491/nikah.0izz62_jmukuu_ia5pdq.webp",
+      date: "Saturday, April 18th 2026",
+      time: "Join Us at 11 am",
+      venue: "The Leela Palace, Udaipur",
+       venue_address:
+        "Bund Garden Road, Agarkar Nagar\nPune, Maharashtra, 411001",
+      theme: "Glitz & Glam: Shimmer, sequins & Statement Fits",
+      link: "https://maps.app.goo.gl/mxcwCxWhH1TXBC8c9",
+    },
+
+
+    {
+      title_ceremony: "Walima",
+      image: "https://res.cloudinary.com/drl4fmhrq/image/upload/v1784716491/walima.001-0bgj0p0i2_qdxyv4.webp",
+      date: "Saturday, April 18th 2026",
+      time: "Join Us at 11 am",
+      venue: "The Leela Palace, Udaipur",
+       venue_address:
+        "Bund Garden Road, Agarkar Nagar\nPune, Maharashtra, 411001",
+      theme: "Glitz & Glam: Shimmer, sequins & Statement Fits",
+      link: "https://maps.app.goo.gl/f599YkTSEYKDEK5L7",
+    },
+  ],
+
+  backgroundMusicUrl: "",
+  backgroundMusicFileName: "",
+  sharePreviewTitle: "FARDEEN & ZARIN Wedding Invitation",
+  sharePreviewDescription:
+    "Join us as we celebrate the wedding of FARDEEN and ZARIN.",
+  sharePreviewImage:
+    "https://res.cloudinary.com/drl4fmhrq/image/upload/v1783145066/og_n_yisygg.jpg",
 }
 
 export default niqahDefaultData;
