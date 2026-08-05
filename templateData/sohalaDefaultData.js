86564f815d996b7a67b90b12dc27d93f4fe7cfef
fardeenpathan <md.fardeen5236@gmail.com>
2026-08-05 12:43:07 +0530

    update sohala template

--- templateData/sohalaDefaultData.js
+++ templateData/sohalaDefaultData.js
@@ -1,5 +1,96 @@
 const sohalaDefaultData = {
+groomName: "SAMPATH",
+  brideName: "SAYALI",
+  religiousMantra: "ॐ श्री गणेशाय नम",
+  groomDetails: "(Son of Shri Amit Kulkarni & Shrimati Vaishali Kulkarni)",
+  blessingMessage: "With the heavenly blessings of",
+  brideGrandParentsName: "Shri Rajesh Kulkarni & Shrimati Sunita Kulkarni",
+  brideDetails: "(Daughter of Shri Mahesh Joshi & Shrimati Asha Joshi)",
+  eventIntro: "On the following events",
+  headline: "INVITES",
+  inviteLine: "you to join us in the wedding celebrations of",
+  thankyoutitle: "With Love From Us",
+  thankyoumessage:
+    "Thank you for being part our journey. Your presence makes this celebration truly meaningful, and we look forward to sharing these cherished moments with you.",
+  coupleMessageTitle: "Introducing",
+  coupleMessageDescription: "The Couple",
+  coupleMessageThingsToKnowTitle: "A Guide for Guests",
+  coupleMessageStaffTitle: "Staff",
+  coupleMessageStaffDetails:
+    "for those traveling from far, Royal Orchid Suites offers a comfortable stay nearby.",
+  coupleMessageWeatherTitle: "Weather",
+  coupleMessageWeatherDetails:
+    "Clouds may drop by uninvited, but so will great vibes and better dance moves. A little rain never stopped a good celebration anyway.",
+  coupleMessageParkingTitle: "Parking",
+  coupleMessageParkingDetails:
+    "Valet parking for all our guests will be available at the venue.",
+  coupleMessageThingsToKnowDescription:
+    "Your presence means the world to us. To make your experience effortless and enjoyable, we've gathered a few useful details below.",
+coupleMessageClosingTitle: "Awaiting the Pleasure of Your Company",
+  coupleMessageRsvpText: "Click on the Whatsapp icon to RSVP",
+  rsvpMode: "whatsapp",
+  rsvpWhatsappButtonText: "Click on the Whatsapp icon to RSVP",
+  rsvpFormButtonText: "Fill RSVP Form",
+  whatsappNumber: "919876543210",
+  rsvpGoogleFormLink: "",
+  marriageCountdownTitle: "The Journey Begins",
+  marriageCountdownDate: "2026-09-21",
+  marriageCountdownDescription:
+    "Surrounded by family and friends, we can't wait to celebrate this beautiful moment with you.",
 
+ coupleMessageImages: {
+  image1: "",
+  image2: "",
+  image3: "",
+  image4: "",
+  
+},
+
+
+  events: [
+    {
+      title_ceremony: "Mehendi",
+      image: "https://res.cloudinary.com/drl4fmhrq/image/upload/v1785907681/mehendi.0bdyqcjcsii00_bhi9lg.webp",
+      date: "Saturday, April 18th 2026",
+      time: "Join Us at 11 am",
+     venue: "The Leela Palace",
+      venue_address: "Bund Garden Road, Agarkar Nagar\nPune, Maharashtra, 411001",
+      theme: "Pretty in Pink: Florals, Pastels & Pink Hues",
+      link: "https://maps.app.goo.gl/53z68ksx4cYgoNm59",
+    },
+
+    {
+      title_ceremony: "Shaadi",
+      image: "https://res.cloudinary.com/drl4fmhrq/image/upload/v1785907681/shaadi.0yl0krxfjej6i_iwf38r.webp",
+      date: "Saturday, April 18th 2026",
+      time: "Join Us at 11 am",
+     venue: "The Leela Palace",
+      venue_address: "Bund Garden Road, Agarkar Nagar\nPune, Maharashtra, 411001",
+      theme: "Pretty in Pink: Florals, Pastels & Pink Hues",
+      link: "https://maps.app.goo.gl/53z68ksx4cYgoNm59",
+    },
+
+
+    {
+      title_ceremony: "Reception",
+      image: "https://res.cloudinary.com/drl4fmhrq/image/upload/v1785907681/reception.0-kw_hq1f.2_i_d4vqaa.webp",
+      date: "Saturday, April 18th 2026",
+      time: "Join Us at 11 am",
+      venue: "The Leela Palace",
+      venue_address: "Bund Garden Road, Agarkar Nagar\nPune, Maharashtra, 411001",
+      theme: "Pretty in Pink: Florals, Pastels & Pink Hues",
+      link: "https://maps.app.goo.gl/53z68ksx4cYgoNm59",
+    },
+  ],
+
+
+   backgroundMusicUrl: "",
+  backgroundMusicFileName: "",
+  sharePreviewTitle: "SAMPATH & SAYALI Wedding Invitation",
+  sharePreviewDescription:
+    "Join us as we celebrate the wedding of SAMPATH and SAYALI.",
+  sharePreviewImage:
+    "https://res.cloudinary.com/drl4fmhrq/image/upload/v1783145066/og_n_yisygg.jpg",
 }
 
 
